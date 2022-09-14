7f8285daff0138d8660d95dcdfff2d02e7f9dd3c
Karzhavin <karzhav1n@yandex.ru>
2022-09-14 14:33:57 +0500

    up ability change text

--- todo-list/scripts/add-item-button.js
+++ todo-list/scripts/add-item-button.js
@@ -7,10 +7,16 @@ const pointList = document.querySelector('.point-list');
 
 addItemButton.addEventListener('click', function() {
   const text = prompt('Enter your text');
-  // create list item
+  // create list item and it content
   const itemText = document.createElement('span');
   itemText.classList.add('list-item-text');
+  itemText.setAttribute('title', 'click for change text');
   itemText.textContent = text;
+  // add functionality of item text
+  itemText.addEventListener('click', function() {
+    const newText = prompt('Enter your new text');
+    itemText.textContent = newText;
+  });
 
   const removeItemButton = document.createElement('button');
   removeItemButton.classList.add('remove-item-button');
